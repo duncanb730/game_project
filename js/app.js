2ce5b0319da2a376d7c150bd98d77546ce24c903
duncanb730 <duncanb730@gmail.com>
2016-09-08 17:55:21 -0700

    comments comments comments

--- js/app.js
+++ js/app.js
@@ -1,71 +1,38 @@
+//This is the speed. I wouldn't recommend changing it.
 var Speed = function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1));
 };
 
-// Enemies our player must avoid
+// The bad guys. They can be changed into something more menacing than a ladybug if you like.
+//You'll have to put a .png of your new enemy into the image folder and put the URL under the this.sprite variable
+//and have all kinds of fun.
 var Enemy = function(x, y, sprite) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
     this.speed = Speed(95, 250);
 };
- // var EnemyRev = function(x, y, sprite) {
- //    this.sprite = 'images/enemy-bug2.png';
- //    this.x = x;
- //    this.y = y;
- //    this.speed = Speed(65, 250);
- // };
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+ 
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     if ( this.x < 808) {
         this.x += this.speed * dt;
     } else {
         this.x = 0;
     }
 };
-// EnemyRev.prototype.update = function(dt) {
-//     if ( this.x < 808) {
-//         this.x += this.speed * dt;
-//     } else {
-//         this.x = 808;
-//     }
-// };
-// Draw the enemy on the screen, required method for game
+
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-// EnemyRev.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+//Here we have your character being created. You can change a few setting here and make
+//a different avatar for yourself. There are several female characters available in the image folder.
+//You can change the 'images/char-boy.png' to anything you like, so long as the image is in the folder.
 var Player = function() {
     this.x = 404;
     this.y = 390;
     this.sprite = 'images/char-boy.png';
 };
-// var collide = function() {
-//     if (Enemy.x && Enemy.y === Player.x && Player.y) {
-//         Player.reset();
-//     }
-// };
-// Player.prototype.collide = function() {
-//     allEnemies.forEach(function(enemy) {
-//         if (Math.abs (enemy.x - Player.x) < 50 && (enemy.y - Player.y) < 50) {
-//             this.reset();
-//         }
-//     });
-// };
 
 Player.prototype.update = function() {
     var self = this;
@@ -79,6 +46,8 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//This involves movement. I suppose you can mess with someone's head by changing parts of this,
+//but why would you want to do that? You really don't need to mess with this part.
 Player.prototype.handleInput = function(direction) {
     if (direction === 'left' && this.x !== borders.left) {
         this.x -= 101;
@@ -95,17 +64,26 @@ Player.prototype.handleInput = function(direction) {
         this.reset();
     }
 };
+
+//BorderTime! You can't leave the board, and this is how we keep you from leaving.
+//These borders can be adjusted if you decide to make the board larger or smaller,
+//but you don't have to monkey with the numbers here otherwise.
  var borders = {
     left: 0,
     right: 707,
     up: 50,
     bottom: 390
  };
+ 
+ //This is what happens when you either win or die. 
+ //Or at least this is what happens in the current iteration of the game. I'll see what might happen later...
  Player.prototype.reset = function(reset) {
     this.x = 404;
     this.y = 390;
  };
-// Now instantiate your objects.
+
+//Here thar be monsters. This is where we add more or less enemies. 
+//It's a two-step process. Step one is making the enemy exist.
 var enemy1 = new Enemy(-101, 55, 'images/enemy-bug.png');
 var enemy2 = new Enemy(-101, 140, 'images/enemy-bug.png');
 var enemy3 = new Enemy(-101, 225, 'images/enemy-bug.png');
@@ -119,7 +97,7 @@ var enemy10 = new Enemy(-101, 140, 'images/enemy-bug.png');
 var enemy11 = new Enemy(-101, 55, 'images/enemy-bug.png');
 var enemy12 = new Enemy(-101, 225, 'images/enemy-bug.png');
 
-// Place all enemy objects in an array called allEnemies
+//Step two is making them show up on the screen.
 var allEnemies = [];
     allEnemies.push(enemy1);
     allEnemies.push(enemy2);
@@ -134,14 +112,13 @@ var allEnemies = [];
     allEnemies.push(enemy11);
     allEnemies.push(enemy12);
 
-// Place the player object in a variable called player
+// This puts your character on the board. You can adjust the numbers to change your start location.
 var player = new Player(404, 390);
 var allPlayers = [];
 allPlayers.push(player);
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This is how you move. Don't mess with it. The game will get either very boring or a little confusing if you do.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
